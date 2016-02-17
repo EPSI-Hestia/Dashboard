ac7ac222d956d2eed7cd0a9affe90c4c69c78a02
Theov <vermerschth@gmail.com>
2016-02-17 20:33:15 +0100

    async refresh

--- controller/routes.js
+++ controller/routes.js
@@ -7,11 +7,11 @@ $('#myTabs a').click(function (e) {
 
 angular.module('dashboard', ['chart.js'])
 
-    .controller('dashboardCtrl', ['$scope','$http', function ($scope, $http){
+    .controller('dashboardCtrl', ['$scope','$http', '$interval', function ($scope, $http, $interval){
         $scope.showAgent = false;
         $scope.valueExist = false;
          $scope.agentValues = {};
-        $scope.url = "http://192.168.1.21:8080/";
+        $scope.url = "http://192.168.1.22:8080/";
 
         $http({method: 'GET', url: $scope.url})
             .then(function successCallback( response) {
@@ -29,47 +29,55 @@ angular.module('dashboard', ['chart.js'])
         }
 
         $scope.listValuesAgent = function(agent) {
-               $scope.currentAgent = agent;
-               $http({
-                   method: 'GET',
-                   url: $scope.url + $scope.currentBoard + '/' + $scope.currentAgent + '/last/10',
-               })
-                   .then(function successsCallback(response) {
-                   $scope.dates = [];
-                   $scope.secondes = [];
-                   $scope.values = [];
+	       $scope.currentAgent = agent;
+    
+	
+        function async_func(){
+  	       $http({
+                     method: 'GET',
+                     url: $scope.url + $scope.currentBoard + '/' + $scope.currentAgent + '/last/10',
+                 })
+                     .then(function successsCallback(response) {
+                     $scope.dates = [];
+                     $scope.secondes = [];
+                     $scope.values = [];
 
-                   angular.forEach(response.data, function (data) {
-                       this.push(parseInt(data.datetime.split("-")[1].split(":")[2]));
-                   }, $scope.secondes);
+                     angular.forEach(response.data, function (data) {
+                         this.push(parseInt(data.datetime.split("-")[1].split(":")[2]));
+                     }, $scope.secondes);
 
-                   getSortSecondes($scope.secondes);
+                     getSortSecondes($scope.secondes);
 
-                   angular.forEach(response.data, function (data) {
-                       this.push(parseFloat(data.value));
-                   }, $scope.values);
+                     angular.forEach(response.data, function (data) {
+                         this.push(parseFloat(data.value));
+                     }, $scope.values);
 
-                   $scope.min = Math.min.apply(Math, $scope.values);
-                   $scope.max = Math.max.apply(Math, $scope.values);
-                   var sum = $scope.values.reduce(function (a, b) {
-                       return a + b;
-                   });
-                   $scope.average = Math.round(sum / $scope.values.length);
-                   $scope.lastValue = $scope.values[$scope.values.length - 1];
-                   $scope.data = [$scope.values];
-                   $scope.labels = $scope.secondes;
-                   $scope.valueExist = true;
-                   $scope.legend = $scope.currentAgent;
-                   $scope.series = ["valeur du capteur"];
-               });
+                     $scope.min = Math.min.apply(Math, $scope.values);
+                     $scope.max = Math.max.apply(Math, $scope.values);
+                     
+  		              var sum = $scope.values.reduce(function (a, b) {
+                         return a + b;
+                     });
 
+                     $scope.average = Math.round(sum / $scope.values.length);
+                     $scope.lastValue = $scope.values[$scope.values.length - 1];
+                     $scope.data = [$scope.values];
+                     $scope.labels = $scope.secondes;
+                     $scope.valueExist = true;
+                     $scope.legend = $scope.currentAgent;
+                     $scope.series = ["valeur du capteur"];
+                 });
+          }
 
          function getSortSecondes(secondes){
                     secondes.sort(function (a, b) {
                         return a - b
                     }).toString().split(",");
                 }
-           }
+
+          $scope.async=$interval(async_func, 1000);
+          $scope.async;
+        }
 
         $scope.refreshDashboard = function(agent){
             $scope.listValuesAgent(agent);
